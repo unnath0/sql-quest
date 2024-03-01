1d98ec242055c3d827ace6ce7dc5c6decee87fca
unnath0 <timeandwind01@gmail.com>
2024-03-02 00:10:16 +0530

    first commit

---
+++ App/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate 
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import UserCreationForm, LoginForm, SignupForm
+
+# Create your views here.
+def home(request):
+  return render(request,'home.html')
+
+def login(request):
+  if request.method == 'POST':
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password']
+      user = authenticate(request, username=username, password=password)
+      if user:
+        auth_login(request, user)
+        return redirect('home')
+  else:
+    form = LoginForm()
+  return render(request, 'login.html', {'form': form})
+
+def signup(request):
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
+    if form.is_valid():
+      form.save()
+      # TODO: Insert the signup form details into database here
+      return redirect('login')
+  else:
+    form = SignupForm()
+  return render(request, 'signup.html', {'form': form})
+
+def logout(request):
+  auth_logout(request)
+  return redirect('login')
+
+# def modules(request):
