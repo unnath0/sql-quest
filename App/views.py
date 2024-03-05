6465209010cac6cb96f84bad39706442ea8ca2d1
unnath0 <timeandwind01@gmail.com>
2024-03-05 07:09:48 +0530

    added user authentication

--- App/views.py
+++ App/views.py
@@ -5,6 +5,17 @@ from django.contrib.auth import logout as auth_logout
 from .forms import UserCreationForm, LoginForm, SignupForm
 import mysql.connector
 
+connection = mysql.connector.connect(
+    host='localhost',
+    database='prototype',
+    user='anand',
+    password='pass'
+)
+
+cursor = connection.cursor()
+
+userID = 0
+
 # Create your views here.
 def home(request):
   return render(request,'home.html')
@@ -18,6 +29,9 @@ def login(request):
       user = authenticate(request, username=username, password=password)
       if user:
         auth_login(request, user)
+        getUserid = f"select UserID from users where username = '{username}'"
+        result = cursor.execute(getUserid)
+        userID = result[0][0] if result else None
         return redirect('modules')
   else:
     form = LoginForm()
@@ -29,6 +43,12 @@ def signup(request):
     if form.is_valid():
       form.save()
       # TODO: Insert the signup form details into database here
+      username = form.cleaned_data.get('username')
+      email = form.cleaned_data.get('email')
+      password = form.cleaned_data.get('password2')
+      add_user = f"INSERT INTO users (Username,Email,PasswordHash) values ('{username}','{email}','{password}')"
+      cursor.execute(add_user)
+      connection.commit()
       return redirect('login')
   else:
     form = SignupForm()
@@ -39,6 +59,7 @@ def logout(request):
   return redirect('login')
 
 def modules(request):
+  # get the {modID: moduleName, completedQuestions} from db
   return render(request, 'modules.html')
 
 def question(request):
